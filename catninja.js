7f72176372f82e7c72e47579310d3fde50a6661e
Mayank <mbapna123@gmail.com>
2017-09-23 02:32:03 -0400

    Added files to catninja portfolio

---
+++ catninja.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $(".one").click(function(){
+    if ($(".one").attr("src")=="img/cat0.png"){
+  $(".one").attr("src","img/ninja0.png")}
+  else{$(".one").attr("src","img/cat0.png")}})
+
+  $(".two").click(function(){
+    if ($(".two").attr("src")=="img/cat1.png"){
+  $(".two").attr("src","img/ninja1.png")}
+  else{$(".two").attr("src","img/cat1.png")}})
+
+  $(".three").click(function(){
+    if ($(".three").attr("src")=="img/cat2.png"){
+  $(".three").attr("src","img/ninja2.png")}
+  else{$(".three").attr("src","img/cat2.png")}})
+
+  $(".four").click(function(){
+    if ($(".four").attr("src")=="img/cat3.png"){
+  $(".four").attr("src","img/ninja3.png")}
+  else{$(".four").attr("src","img/cat3.png")}})
+
+  $(".five").click(function(){
+    if ($(".five").attr("src")=="img/cat4.png"){
+  $(".five").attr("src","img/ninja4.png")}
+  else{$(".five").attr("src","img/cat4.png")}})
+  })
